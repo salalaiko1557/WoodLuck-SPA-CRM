2203563acc3405820816cde388397468ec80a6cd
ALEX SALALAIKO <salalaiko1557@gmail.com>
2019-05-11 17:01:43 +0300

    refactor order number

--- database/migrations/2019_05_11_132641_add_order_number_column_to_orders_table.php
+++ database/migrations/2019_05_11_132641_add_order_number_column_to_orders_table.php
@@ -14,7 +14,7 @@ class AddOrderNumberColumnToOrdersTable extends Migration
     public function up()
     {
         Schema::table('orders', function (Blueprint $table) {
-            $table->integer('order_number')->nullable();
+            $table->string('order_number')->nullable();
         });
     }
 
